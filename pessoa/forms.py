01db67412cc22919f21f5e49a3101022f3bf3dd2
ClaudioFritzen <sdfritzen96@gmail.com>
2022-10-22 20:33:01 +0100

    apenas colando

---
+++ pessoa/forms.py
@@ -0,0 +1,19 @@
+from django import forms
+from django.forms import fields
+from .models import Pessoa, Contato
+
+class PessoaForm(forms.ModelForm):
+    data_nascimento = forms.DateField(
+        widget=forms.TextInput(
+            attrs={"type": "date"}
+        )
+    )
+    class Meta:
+        model = Pessoa
+        fields = ['nome_completo', 'data_nascimento', 'ativa']
+
+
+class ContatoForm(forms.ModelForm):
+    class Meta:
+        model = Contato
+        fields = ['nome', 'email', 'telefone']
